6a3aa0f53ed7162219e0c46fd6e4e88823b58200
Gabriel Duquette <gabrielduquette@gmail.com>
2018-12-19 19:42:27 -0500

    data imported from OEC and cpi.csv

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { csv } from 'd3-fetch';
 import './App.css';
 
 class App extends Component {
+  state = {
+    oilData: null,
+    cpi: null
+  }
+
+  async componentDidMount() {
+    const cpi = await csv("cpi.csv");
+    axios.get("https://atlas.media.mit.edu/hs92/export/2016/ago/show/2709/")
+      .then(response => {
+        this.setState({ oilData: response.data.data, cpi });
+      })
+      .catch(error => console.log(error));
+  }
+
+  totalExportValue = (arr) => {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].export_val) {
+        total += arr[i].export_val;
+      }
+    }
+    return total;
+  }
+
   render() {
+    if (this.state.cpi) { console.log(this.state.cpi.find(x => x.Jurisdiction === "Angola").Jurisdiction); }
+    if (this.state.oilData) {
+      return (
+        <div>
+          <div>
+            {this.totalExportValue(this.state.oilData)}
+          </div>
+          <div>
+            {JSON.stringify(this.state.oilData)}
+          </div>
+          <div>
+            {this.state.cpi.find(x => x.Jurisdiction === "Angola").Jurisdiction}
+          </div>
+          <div>
+            {JSON.stringify(this.state.cpi)}
+          </div>
+        </div>
+      );
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div>Loading...</div>
+    )
   }
 }
 
