cf1411ae12ed92962237e727229a9d5f6c29783e
Gabriel Duquette <gabrielduquette@gmail.com>
2018-12-23 00:40:51 -0500

    add Map component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React, { Component } from 'react';
+import Map from './Map';
 import axios from 'axios';
 import { csv } from 'd3-fetch';
-import {
-  ComposableMap,
-  ZoomableGroup,
-  Geographies,
-  Geography,
-} from 'react-simple-maps';
 import './App.css';
-
-const wrapperStyles = {
-  width: "100%",
-  maxWidth: 980,
-  margin: "0 auto",
-}
 
 class App extends Component {
   state = {
@@ -23,7 +12,7 @@ class App extends Component {
 
   async componentDidMount() {
     const cpi = await csv("cpi.csv");
-    axios.get("https://atlas.media.mit.edu/hs92/export/2016/ago/show/2709/")
+    axios.get("https://atlas.media.mit.edu/hs92/export/2016/all/show/2709/")
       .then(response => {
         this.setState({ oilData: response.data.data, cpi });
       })
@@ -41,6 +30,7 @@ class App extends Component {
   }
 
   handleClick(geography, evt) {
+    console.log('Event: ', evt);
     console.log("Geography data: ", geography.properties.name);
   }
 
@@ -50,53 +40,7 @@ class App extends Component {
     if (oilData && cpi) {
       return (
         <div>
-          <div style={wrapperStyles}>
-            <ComposableMap
-              projectionConfig={{
-                scale: 205,
-                rotation: [-11,0,0],
-              }}
-              width={980}
-              height={551}
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-              >
-              <ZoomableGroup center={[0,20]} disablePanning>
-                <Geographies geography="/world-50m.json">
-                  {(geographies, projection) => geographies.map((geography, i) => geography.id !== "ATA" && (
-                    <Geography
-                      key={i}
-                      geography={geography}
-                      projection={projection}
-                      onClick={this.handleClick}
-                      style={{
-                        default: {
-                          fill: "#ECEFF1",
-                          stroke: "#607D8B",
-                          strokeWidth: 0.75,
-                          outline: "none",
-                        },
-                        hover: {
-                          fill: "#607D8B",
-                          stroke: "#607D8B",
-                          strokeWidth: 0.75,
-                          outline: "none",
-                        },
-                        pressed: {
-                          fill: "#FF5722",
-                          stroke: "#607D8B",
-                          strokeWidth: 0.75,
-                          outline: "none",
-                        },
-                      }}
-                    />
-                  ))}
-                </Geographies>
-              </ZoomableGroup>
-            </ComposableMap>
-          </div>
+          <Map handleClick={this.handleClick}/>
           <div>
             {this.totalExportValue(oilData)}
           </div>
