cb84734f7546144db9a3e9e8dec1ac5154f496c3
Gabriel Duquette <gabrielduquette@gmail.com>
2018-12-24 01:24:12 -0500

    convert csvs into countries.js, some country codes missing

--- src/App.js
+++ src/App.js
@@ -2,56 +2,37 @@ import React, { Component } from 'react';
 import Map from './components/Map';
 import Chart from './components/Chart';
 import axios from 'axios';
-import { csv } from 'd3-fetch';
+import countries from './reference/countries';
 import './App.css';
 
 class App extends Component {
   state = {
-    oilData: null,
-    cpi: null,
-    countries: null
+    oilData: null
   }
 
-  async componentDidMount() {
-    const cpi = await csv("cpi.csv");
-    const countries = await csv("locations_international_atlas.csv");
-    axios.get("https://atlas.media.mit.edu/hs92/export/2016/all/show/2709/")
+  getOilData = (countryCode) => {
+    console.log(countryCode);
+    axios.get(`https://atlas.media.mit.edu/hs92/export/1998.2015/${countryCode.toLowerCase()}/show/2709/`)
       .then(response => {
-        this.setState({ oilData: response.data.data, cpi, countries });
+        // strip data function here? probably unnecessary
+        this.setState({ oilData: response.data.data });
       })
       .catch(error => console.log(error));
   }
 
-  handleClick = (geography, evt) => {
-    console.log('Event: ', evt);
-    console.log("Geography data: ", geography.properties.name);
+  handleClick = (geography, e) => {
+    e.preventDefault();
+    const country = countries.find(country => country.name === geography.properties.name)
+    this.getOilData(country.code);
   }
 
   render() {
-    const { oilData, cpi, countries } = this.state;
-
-    if (oilData && cpi) {
-      return (
-        <div>
-          <Map handleClick={this.handleClick}/>
-          <div>
-            {JSON.stringify(oilData)}
-          </div>
-          <div>
-            {cpi.find(x => x.Jurisdiction === "Angola").Jurisdiction}
-          </div>
-          <div>
-            {JSON.stringify(countries)}
-          </div>
-          <div>
-            {JSON.stringify(cpi)}
-          </div>
-        </div>
-      );
-    }
-
+    const { oilData } = this.state;
     return (
-      <div>Loading...</div>
+      <div>
+        <Map handleClick={this.handleClick}/>
+        {oilData ? <div>{JSON.stringify(oilData)}</div> : null}
+      </div>
     );
   }
 }
